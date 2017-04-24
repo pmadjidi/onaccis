ad44836683c1fe6eb917253558aa8bcddf692099
Payam Madjidi <payam@dhcp-31-162.ite.kth.se>
2017-04-24 10:36:27 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -4,6 +4,7 @@ var bcrypt = require('bcrypt');
 function process(message,client){
   console.log("Login message recieved",message)
 var salt = bcrypt.genSaltSync(128);
+console.log({salt})
 var hash = bcrypt.hashSync(message.password, salt)
 console.log({salt,hash})
 }
