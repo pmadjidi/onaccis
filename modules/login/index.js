1bda962e9f99b58f9f01e16983db189956912b5c
Payam Madjidi <payam@dhcp-31-162.ite.kth.se>
2017-04-24 10:18:51 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -12,7 +12,7 @@ function process(message,client){
 function hashPassword(password) {
     var salt = crypto.randomBytes(128).toString('base64');
     var iterations = 10000;
-    var hash = pbkdf2(password, salt, iterations);
+    var hash = crypto.pbkdf2(password, salt, iterations);
 
     return {
         salt: salt,
@@ -22,7 +22,7 @@ function hashPassword(password) {
 }
 
 function isPasswordCorrect(savedHash, savedSalt, savedIterations, passwordAttempt) {
-    return savedHash == pbkdf2(passwordAttempt, savedSalt, savedIterations);
+    return savedHash == crypto.pbkdf2(passwordAttempt, savedSalt, savedIterations);
 }
 
 module.exports = {
