8d82d7c3cc952e809bf1c2225f8b308795e0018c
Payam Madjidi <payam@dhcp-31-162.ite.kth.se>
2017-04-24 12:33:39 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -12,7 +12,7 @@ function process(message,client){
   console.log("Login message recieved",message)
   DB.collection("user").findOne({username: message.username})
   .then(user=>console.log(user))
-  .catch(err=>console.log(err))
+  .catch(err=>console.log("No User "+err))
 
 let salt = crypto.randomBytes(128).toString('hex')
 let result = sha512(message.password,salt)
