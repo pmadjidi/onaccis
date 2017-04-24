162953f4971df5734758437ca42575346b4644e7
Payam Madjidi <payam@dhcp-31-162.ite.kth.se>
2017-04-24 11:30:09 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -1,8 +1,19 @@
 "use strict"
-var crypto = require('crypto');
+
+const crypto = require('crypto');
+const MongoClient = require('mongodb').MongoClient;
+let dbUrl = "mongodb://localhost:27017/users"
+let DB = null
+MongoClient.connect(dbUrl)
+  .then(db=> DB = db)
+  .catch(err=>console.log("Error Connecting to database " + dbUrl + err))
 
 function process(message,client){
   console.log("Login message recieved",message)
+  DB.collection.findOne({username: message.username})
+  .then(user=>console.log(user))
+  .catch(err=>console.log(err))
+  
 let salt = crypto.randomBytes(128).toString('hex')
 let result = sha512(message.password,salt)
 console.log(result)
