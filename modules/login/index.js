01d58180c39129bb0b6508b2a1d521532df810c7
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-04-25 07:00:54 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -8,6 +8,16 @@ MongoClient.connect(dbUrl)
   .then(db=>{console.log("Connected to database users"); DB = db})
   .catch(err=>console.log("Error Connecting to database " + dbUrl + err))
 
+function setSession(username,sesssion) {
+    return DB.collection("sesssion").insert(
+   {
+     username: username,
+     session: session
+     time: new Date().getTime()
+   })
+}
+
+
 function process(message,client){
   console.log("Login message recieved",message)
   DB.collection("user").findOne({username: message.username})
@@ -19,26 +29,31 @@ function process(message,client){
   })
   .then(user=>{
     let hash = sha512(message.password,user.salt)
-    if (hash === user.hash)
-      client.send(JSON.stringify({auth: "ok",user: message.username}))
+    if (hash === user.hash) {
+      let session = crypto.randomBytes(128).toString('hex')
+      setSession(message.username,session)
+      return client.send(JSON.stringify({auth: "true",user: message.username, session: session}))
+    }
     else {
-        client.send(JSON.stringify({auth: "denied"}))
+        return client.send(JSON.stringify({auth: "false", user: message.username}))
     }
   })
   .catch(err=>{
     console.log("No User "+err)
   console.log("Creating user......")
+let session = crypto.randomBytes(128).toString('hex')
 let salt = crypto.randomBytes(128).toString('hex')
 let result = sha512(message.password,salt)
 console.log("Result = " + result)
-DB.collection("user").insert(
+return DB.collection("user").insert(
    {
      username: message.username,
      hash:  result.hash,
      salt: result.salt,
      time: new Date().getTime()
    })
-   .then(client.send(JSON.stringify({auth: "ok", user: message.username})))
+   .then(setSession(message.username,session))
+   .then(client.send(JSON.stringify({auth: "true", user: message.username})))
 })
 }
 
