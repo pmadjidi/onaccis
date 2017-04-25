6d941c227562c17fbee601e7353ef4f91dcf827c
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-04-25 09:19:35 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -17,7 +17,7 @@ MongoClient.connect(sessionUrl)
 
 
 
-function setSession(username,sesssion) {
+function setSession(username,session) {
   let timestamp =  new Date().getTime() + 24 * 60 * 60 * 1000
     return sessionDb.collection("sessions").insert(
    {
