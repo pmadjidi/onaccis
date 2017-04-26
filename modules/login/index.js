71b45cbbdd82d327709a9215ea2dc90b2cc30f8c
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-04-26 06:57:38 +0200

    yet an other commit.....

--- modules/login/index.js
+++ modules/login/index.js
@@ -41,7 +41,7 @@ function setSession(username,session,client) {
     session: session,
     valid:  new Date().getTime() + 24 * 60 * 60 * 1000
   }
-  AUTH.session = sesObj
+  AUTH[sesssion] = sesObj
   console.log(AUTH)
   client.onacciSession = sesObj
 }
