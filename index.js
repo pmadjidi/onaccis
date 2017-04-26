4782deaace4263ef5ccfa059c7fd12e41938d7fd
Payam Madjidi <payam@dhcp-31-162.ite.kth.se>
2017-04-26 09:37:27 +0200

    yet an other commit.....

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ else {
 
 function processOnline(message,client) {
   let userList = []
-  console.log("clients",wss.clients)
-  console.log("number of clients",wss.clients.length)
+  console.log("clients",wss.client)
+  console.log("number of clients",wss.client.length)
   return client.send(JSON.stringify({online: []}))
 
   var i = 0, n = wss.clients ? wss.clients.length : 0, client = null;
