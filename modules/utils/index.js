491e8e0aed491a3ae16754dadea33a3ccc3cb5f2
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-06-20 10:24:31 +0200

    yet an other commit.....

---
+++ modules/utils/index.js
@@ -0,0 +1,13 @@
+"use strict"
+CL(string) {
+    if (string)
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    else {
+        return string
+    }
+}
+
+
+module.exports = {
+  CL
+}
